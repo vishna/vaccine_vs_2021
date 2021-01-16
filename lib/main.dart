2b6a9206ae4d41d88bd1dd3b2e0c453d0adb3f19
Lukasz Wisniewski <luke.cherrish@gmail.com>
2021-01-16 12:08:42 +0100

    add url_strategy, remove # from path

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:url_launcher/url_launcher.dart' as url_launcher;
+import 'package:url_strategy/url_strategy.dart';
 import 'package:voyager/voyager.dart';
 
 final paths = loadPathsFromYamlSync('''
@@ -21,6 +22,7 @@ final plugins = [
 final appKey = GlobalKey<_VaccineVs2021AppState>();
 
 void main() async {
+  setPathUrlStrategy();
   runApp(VaccineVs2021App(
       key: appKey, router: VoyagerRouter.from(paths, plugins)));
 }
